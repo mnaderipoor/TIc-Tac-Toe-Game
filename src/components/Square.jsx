da3ee8d691138201878b9c09cfefaa349e4d1c4c
Mahin <m.naderipoor@ymail.com>
2020-03-29 17:28:27 +0430

    we call .slice() to create a copy of the squares array to modify instead of modifying the existing array

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,15 +1,11 @@
 //The Square component renders a single <button> 
 import React from 'react'
 export default class Square extends React.Component{
-constructor(props){
-    super(props);
-    this.state={
-        value:null
-    }
-}
     render(){
         return(
-            <button onClick={function(){alert('click')}} className="square">
+            <button 
+            onClick={this.props.onClick()} 
+            className="square">
                 {this.props.value}
             </button>
         )

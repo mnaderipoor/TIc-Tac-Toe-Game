51fd02467ec7e3b5e290e0761e738d03833d4fe1
Mahin <m.naderipoor@ymail.com>
2020-03-29 16:19:08 +0430

    declare 3 components and create connection among them by calling that component in another component and passing props

---
+++ README.txt
@@ -0,0 +1,2 @@
+# TIc-Tac-Toe-Game
+this is a tic-tac-toe game to learn react
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,1 +1,12 @@
 //The Square component renders a single <button> 
+import React from 'react'
+export default class Square extends React.Component{
+
+    render(){
+        return(
+            <button className="square">
+                {this.props.value}
+            </button>
+        )
+    }
+}

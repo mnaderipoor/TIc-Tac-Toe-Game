daefe2c5d8d4c39d2c92e83c14d89fc1b09a5b62
Mahin <m.naderipoor@ymail.com>
2020-03-29 16:32:41 +0430

    To “remember” things, components use state.

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,15 @@
 //The Square component renders a single <button> 
 import React from 'react'
 export default class Square extends React.Component{
-
+constructor(props){
+    super(props);
+    this.state={
+        value:null
+    }
+}
     render(){
         return(
-            <button className="square">
+            <button onClick={function(){alert('click')}} className="square">
                 {this.props.value}
             </button>
         )

2a6b290ffb59dac0fdbf653e4346291a1e989fd7
Mahin <m.naderipoor@ymail.com>
2020-03-29 18:42:32 +0430

    ignoring a click if someone has won the game or if a Square is already filled

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -14,6 +14,9 @@ export default class Board extends React.Component{
     handleClick(i){
         // we call .slice() to create a copy of the squares array to modify instead of modifying the existing array
         const squares = this.state.squares.slice();    
+        if (this.calculateWinner(squares) || squares[i]) {      
+            return;    
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';    
         this.setState({squares: squares, xIsNext: !this.state.xIsNext,});
         //There are generally two approaches to changing data. 
@@ -28,9 +31,33 @@ export default class Board extends React.Component{
             onClick={()=> this.handleClick(i)}
             />);
     }
-
+    calculateWinner(squares) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+          }
+        }
+        return null;
+      }
     render() {
-        const status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
+        const winner = this.calculateWinner(this.state.squares);    
+        let status;    
+        if (winner) {      
+            status = 'Winner: ' + winner;    
+        } else {      
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');    
+        }
     
         return (
           <div>
